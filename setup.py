cf07263188cf621fc16fb4f1715a7cbf5bfe92e3
Simon Pfeifer <sikon08@hotmail.co.uk>
2019-05-02 16:49:40 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cprint",
+    version="1.0.0",
+    author="Simon Pfeifer",
+    author_email="s.pfeifer@2012.ljmu.ac.uk",
+    description="Small python package for printing text in different colours and typography.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SimonPfeifer/cprint",
+)
